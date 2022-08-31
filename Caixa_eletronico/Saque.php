6175378a7db95496d5ec6f2a14a69cb00037e963
Luiz Fellipe <98265702+luizf7llipe@users.noreply.github.com>
2022-08-31 17:55:48 -0300

    Add files via upload

---
+++ Caixa_eletronico/Saque.php
@@ -0,0 +1,96 @@
+﻿
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Saque</title>
+     <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<style>
+	body{
+	 background-color: rgb(200,200,200);
+	}
+</style>
+<body>
+
+	<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					
+				</div>
+				<div class="col-md-4" align="center">
+					<label><h3>Valor:</h3></label>
+
+<?php  
+
+$agencia = "";
+$conta = "";
+$senha = "";
+$dinheiro = 0;
+
+
+if (isset($_POST["conta"]) && isset($_POST["senha"]) && isset($_POST["dinheiro"])) {
+	$agencia = $_POST["agencia"];
+    $conta = $_POST["conta"];
+    $senha = $_POST["senha"];
+    $dinheiro = $_POST["dinheiro"];
+} else {
+	header("Location:index.php");
+}
+
+$dinheiroinicial = $dinheiro;
+
+for ($cont1=0;$dinheiro >= 100; $cont1++) { 
+	$dinheiro = $dinheiro - 100;
+}
+
+for ($cont2=0;$dinheiro >= 50; $cont2++) { 
+	$dinheiro = $dinheiro - 50;
+}
+
+for ($cont3=0;$dinheiro >= 20; $cont3++) { 
+	$dinheiro = $dinheiro - 20;
+}
+
+for ($cont4=0;$dinheiro >= 10; $cont4++) { 
+	$dinheiro = $dinheiro - 10;
+}
+
+for ($cont5=0;$dinheiro >= 5; $cont5++) { 
+	$dinheiro = $dinheiro - 5;
+}
+
+for ($cont6=0;$dinheiro >= 1; $cont6++) { 
+	$dinheiro = $dinheiro - 1;
+}
+
+
+echo "".$dinheiroinicial."<br>";
+
+echo "<img src= notas/100reais.jpg width=300px>".$cont1."<br><br>";
+echo "<img src= notas/50reais.jpg width=300px>".$cont2."<br><br>";
+echo "<img src= notas/20reais.jpg width=300px>".$cont3."<br><br>";
+echo "<img src= notas/10reais.jpg width=300px>".$cont4."<br><br>";
+echo "<img src= notas/5reais.jpg width=300px>".$cont5."<br><br>";
+echo "<img src= notas/1real.jpg width=300px>".$cont6."<br><br>";
+
+
+?>
+
+			    </div>
+			</div>
+	</div>
+
+</body>
+
+</html>
---
+++ Caixa_eletronico/index.php
@@ -0,0 +1,60 @@
+﻿
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Banco</title>
+     <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<style>
+	body{
+	 background-color: rgb(200,200,200);
+	}
+</style>
+<body>
+
+	<form action="saque.php" method="post">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					
+				</div>
+				<div class="col-md-4" align="center">
+                    
+                    <label><h3>BANCO 24 HORAS!</h3></label>
+
+					<label><h4>Selecione sua Agência:</h4></label><br>
+
+					<select name="agencia" class="form-control">
+						<option value="1">Caixa</option>
+						<option value="2">Banco do Brasil</option>
+						<option value="3">Itau</option>
+						<option value="4">Santander</option>
+						<option value="5">Bradesco</option>
+					</select>
+                    
+                    <label><h4>Digite sua Conta:</h4></label><br>
+                    <input type="txt" name="conta" class="form-control">
+
+                    <label><h4>Digite sua Senha:</h4></label><br>
+                    <input type="password" name="senha" class="form-control">
+
+                    <label><h4>Quantidade Dinheiro Desejado:</h4></label><br>
+                    <input type="number" name="dinheiro" class="form-control"><br>                                 
+					<input type="submit" class="btn btn-success" name="Sacar" value="Sacar">	
+					<input type="reset"  class="btn btn-danger" name="Cancelar" value="Cancelar">
+
+				</div>
+			</div>
+		</div>
+	</form>
+	
+
+</body>
+
+</html>
